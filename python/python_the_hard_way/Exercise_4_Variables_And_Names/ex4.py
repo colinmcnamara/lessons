f23a20424a526beddb40b76383ab84400280e04c
Colin McNamara <colin@2cups.com>
2014-01-07 07:31:02 -0800

    ex.5

--- python/python_the_hard_way/Exercise_4_Variables_And_Names/ex4.py
+++ python/python_the_hard_way/Exercise_4_Variables_And_Names/ex4.py
@@ -1,10 +1,18 @@
+# Variable_Comment
 cars = 100
-space_in_a_car = 4.0
+# Variable_Comment
+space_in_a_car = 4
+# Variable_Comment
 drivers = 30
+# Variable_Comment
 passengers = 90
+# Variable_Comment
 cars_not_driven = cars - drivers
+# Variable_Comment
 cars_driven = drivers
+# Variable_Comment
 carpool_capacity = cars_driven * space_in_a_car
+# Variable_Comment
 average_passengers_per_car = passengers / cars_driven
 
 print "There are ", cars, "cars available."
@@ -17,3 +25,8 @@ print "We need to put about", average_passengers_per_car, "in each car."
 # Excersize
 # in line 8 (in the example) carpool_capacity was replaced with car_pool_capacity
 # since that variable was not defined, the script errored out
+# 1. nothing thinchage
+# 2. floating points don't round down
+# 3. Comments above each variable
+# 4. = maps a value to a variable
+# 5. _ underscore

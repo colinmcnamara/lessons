789e030d7204a67046789b7dfbe488d3f613155e
Colin McNamara <colin@2cups.com>
2014-01-08 07:15:57 -0800

    ex5 lessons done

--- python/python_the_hard_way/Exercise_5_More_Variables_And_Printing/ex5.py
+++ python/python_the_hard_way/Exercise_5_More_Variables_And_Printing/ex5.py
@@ -5,10 +5,14 @@ weight = 180 # lbs
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
+height_centimeters = (height * 2.54)
+weight_kilogram = (weight * 0.453592)
 
 print "Let's talk about %s." % name
 print "He's %f inches tall." % height
+print "He's %f centimeters tall." % height_centimeters
 print "He's %f pounds heavy." % weight
+print "He's %f kilograms heavy." % weight_kilogram
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth

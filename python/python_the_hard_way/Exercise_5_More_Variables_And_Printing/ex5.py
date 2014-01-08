35997a9755dbd17866dab85f95f8dac9710635a6
Colin McNamara <colin@2cups.com>
2014-01-08 07:08:12 -0800

    ex5 task 2 - changed string formatting modulo to floating point decimal

--- python/python_the_hard_way/Exercise_5_More_Variables_And_Printing/ex5.py
+++ python/python_the_hard_way/Exercise_5_More_Variables_And_Printing/ex5.py
@@ -7,12 +7,12 @@ teeth = 'White'
 hair = 'Brown'
 
 print "Let's talk about %s." % name
-print "He's %d inches tall." % height
-print "He's %d pounds heavy." % weight
+print "He's %f inches tall." % height
+print "He's %f pounds heavy." % weight
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d I get %d." % (
+print "If I add %f, %f, and %f I get %f." % (
 		age, height, weight, age + height + weight)

d4fb589a6bb884e743e48cc87163b63ab9cc6033
Colin McNamara <colin@2cups.com>
2014-01-09 12:10:42 -0800

    started merge / migrate

---
+++ python/python_the_hard_way.asciidoc/Exercise_3_Numbers_And_Math/ex3.py
@@ -0,0 +1,30 @@
+# Printing Text
+print "I will now count my chickens:"
+
+# Addition then division
+print "Hens", 25.0 + 30.0 / 6.0
+
+# Subtraction, Multiplication then take 225 which is 
+#	(100 - 25) * 3 and divides it by 4, and then prints the remainder
+
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+# Printing Text
+print "Now I will count the eggs:"
+# 
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3.0 + 2.0 < 5.0 - 7.0
+
+print "What is 3 + 2?", 3.0 + 2.0
+print "What is 5 - 7?", 5.0 - 7.0
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
---
+++ python/python_the_hard_way.asciidoc/Exercise_4_Variables_And_Names/ex4.py
@@ -0,0 +1,48 @@
+# Variable_Comment
+cars = 100
+# Variable_Comment
+space_in_a_car = 4
+# Variable_Comment
+drivers = 30
+# Variable_Comment
+passengers = 90
+# Variable_Comment
+cars_not_driven = cars - drivers
+# Variable_Comment
+cars_driven = drivers
+# Variable_Comment
+carpool_capacity = cars_driven * space_in_a_car
+# Variable_Comment
+average_passengers_per_car = passengers / cars_driven
+
+print "There are ", cars, "cars available."
+print "there are only", drivers, "drivers available."
+print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car."
+
+# Excersize
+# in line 8 (in the example) carpool_capacity was replaced with car_pool_capacity
+# since that variable was not defined, the script errored out
+# 1. nothing thinchage
+# 2. floating points don't round down
+# 3. Comments above each variable
+# 4. = maps a value to a variable
+# 5. _ underscore
+# 6. -
+# Python 2.7.5 (default, Aug 25 2013, 00:04:04) 
+# [GCC 4.2.1 Compatible Apple LLVM 5.0 (clang-500.0.68)] on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> x = 1
+# >>> y = 2
+# >>> x = 3
+# >>> x * y * z
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# NameError: name 'z' is not defined
+# >>> z = 3
+# >>> x * y * z
+# 18
+# >>> 
+#

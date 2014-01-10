f67c754cd26037728b6285fd722955b489b5fd18
Colin McNamara <colin@2cups.com>
2014-01-10 07:59:41 -0800

    double quote vs single quote in don't

--- python/python_the_hard_way/Exercise_8_Printing,_Printing/ex8.py
+++ python/python_the_hard_way/Exercise_8_Printing,_Printing/ex8.py
@@ -3,10 +3,10 @@ formatter = "%r %r %r %r"
 print formatter % (1, 2, 3, 4)
 print formatter % ("one", "two", "three", "four")
 print formatter % (True, False, False,True)
-print formatter % (formatter, formatter,formatter, formatter)
+print formatter % (formatter, formatter, formatter, formatter)
 print formatter % (
 	"I had this thing.",
 	"That you could type up right.",
-	"But it didn't sing.",
-	"So I said goonight."
+	"But it didnt sing.",
+	"So I said goonight.",
 	)

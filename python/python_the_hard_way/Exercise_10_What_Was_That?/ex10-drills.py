923bc39660d18ebee8ede12b015a9db83ac0f7cb
Colin McNamara <colin@2cups.com>
2014-01-16 08:29:16 -0800

    personal excersize 2

---
+++ python/python_the_hard_way/Exercise_10_What_Was_That?/ex10-drills.py
@@ -0,0 +1,15 @@
+tabbed_car = "\t I have a tab on my rooof"
+split_car = "i'm split\in half"
+slashed_car = " I'm a car that is split into back \ middle \ front"
+
+giant_car = """
+I have the following parts:
+\t * bumper
+\t * hood
+\t * drivers compartment
+\t * tires\n\t * wheels
+"""
+print tabbed_car
+print split_car
+print slashed_car
+print giant_car

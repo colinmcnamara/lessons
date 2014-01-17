25c045d437a4c981f7a82432f7f3ee716a94750e
Colin McNamara <colin@2cups.com>
2014-01-17 15:38:57 -0800

    ex15.5

--- python/python_the_hard_way/Exercise_15_Reading_Files/ex15.py
+++ python/python_the_hard_way/Exercise_15_Reading_Files/ex15.py
@@ -2,7 +2,8 @@
 from sys import	argv
 
 #scriptname.py argument (the argument will be stored as argv)
-script, filename = argv
+print "Enter the filename you want processed"
+filename = raw_input("> ")
 
 #variable txt is set to open the filename passed as the first argument
 txt = open(filename)
@@ -11,13 +12,3 @@ print "Here's your file %r:" % filename
 # read the text file (txt(txt=thefilename to open) read) print that out
 print txt.read()
 
-# ask the user to type the file name again
-print "Type the filename again:"
-# take the input, store it as file again
-file_again = raw_input("> ")
-
-#fill the txt_again variable with the file that is opened
-txt_again = open(file_again)
-
-#read the file, print it to the screen
-print txt_again.read()

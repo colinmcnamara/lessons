31dc664225a4208dd408625662230e5edaea5d60
Colin McNamara <colin@2cups.com>
2014-01-18 07:29:57 -0800

    ex17.3

--- python/python_the_hard_way/Exercise_17_More_Files/ex17.py
+++ python/python_the_hard_way/Exercise_17_More_Files/ex17.py
@@ -6,8 +6,8 @@ script, from_file, to_file = argv
 print "Copying from %s to %s" % (from_file, to_file)
 
 #we could do these ttwo on one line too, how?
-in_file = open(from_file)
-indata = in_file.read()
+indata = open(from_file).read()
+#indata = in_file.read()
 
 #print "The input file is %d bytest long" % len(indata)
 
@@ -21,4 +21,4 @@ out_file.write(indata)
 #print "Alright, all done."
 
 out_file.close()
-in_file.close()
+#in_file.close()

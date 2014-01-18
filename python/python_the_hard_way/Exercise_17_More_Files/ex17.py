2a9362307b4ffdc3dc79532ac71cd65aebf80458
Colin McNamara <colin@2cups.com>
2014-01-18 07:26:54 -0800

    shortened ex17

--- python/python_the_hard_way/Exercise_17_More_Files/ex17.py
+++ python/python_the_hard_way/Exercise_17_More_Files/ex17.py
@@ -9,16 +9,16 @@ print "Copying from %s to %s" % (from_file, to_file)
 in_file = open(from_file)
 indata = in_file.read()
 
-print "The input file is %d bytest long" % len(indata)
+#print "The input file is %d bytest long" % len(indata)
 
-print "Does the output file exist? %r " % exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C to abort."
-raw_input()
+#print "Does the output file exist? %r " % exists(to_file)
+#print "Ready, hit RETURN to continue, CTRL-C to abort."
+#raw_input()
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
 
-print "Alright, all done."
+#print "Alright, all done."
 
 out_file.close()
 in_file.close()

fa466d17c4b11646c7a13ed1918b34efecb6893f
Colin McNamara <colin@2cups.com>
2014-01-18 16:20:50 -0800

    finished excersize 21

--- python/python_the_hard_way/Exercise_21_Functions_Can_Return_Something/ex21.py
+++ python/python_the_hard_way/Exercise_21_Functions_Can_Return_Something/ex21.py
@@ -6,7 +6,7 @@ def subtract(a, b):
 	print "SUBTRACTING %d - %d" % (a, b)
 	return a - b
 
-def mulitply(a,b):
+def multiply(a,b):
 	print "MULTIPLYING %d * %d" % (a, b)
 	return a * b
 
@@ -18,7 +18,7 @@ print "let's do some math with just functions!"
 
 age = add(30, 5)
 height = subtract(78, 4)
-weight = mulitply(90, 2)
+weight = multiply(90, 2)
 iq = divide(100, 2)
 
 print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight,iq)
@@ -26,6 +26,16 @@ print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight,iq)
 # a puzzle for extra credit, type it in anyways.
 print "Here is a puzzle."
 
-what = add(age, subtract(height, mulitply(weight, divide(iq, 2))))
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
 print "That become: ", what, "Can you do it by hand?"
+
+team_members = 4
+cost_per = 100000
+margin = 1000000
+team_share = 0.1
+
+individual_share = multiply(team_share, divide(subtract(margin, multiply(cost_per, team_members)), team_members))
+
+
+print "Individual bonus = ", individual_share, "Can you do it by hand?"

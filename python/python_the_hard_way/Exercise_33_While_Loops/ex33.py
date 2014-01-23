47d26493af3a2b1286ca2ca3047d917f982c39f8
Colin McNamara <colin@2cups.com>
2014-01-23 06:38:53 -0800

    ex33 intial + commenting

---
+++ python/python_the_hard_way/Exercise_33_While_Loops/ex33.py
@@ -0,0 +1,25 @@
+i = 0
+# because we put brackets [] at the end of the numbers "variable" i
+# now becomes a "list"
+numbers = []
+
+# while the variable i is less then 6
+while i < 6:
+	#print the number at the top of the list
+	print "At the top i is %d" % i
+	# append the list numbers w/ the contents of variable "i"
+	numbers.append(i)
+# increment "i" by one. could also use i += 1
+	i = i + 1
+# print out numbers now : and then return the list numbers
+	print "Numbers now: ", numbers
+# print out the bottom of the list (this looks like a safety net to catch a endless loop)
+	print "At the bottom i is %d" % i
+
+# print to screen
+print "The numbers: "
+
+# loop through the numbers list. Assign each element to "num"
+for num in numbers:
+	#print out num (one stagement each for loop)
+	print num

38f430fef25379f55ca20df716345d1ef9ecfd4f
Colin McNamara <colin@2cups.com>
2014-01-23 08:04:17 -0800

    finished ex33

--- python/python_the_hard_way/Exercise_33_While_Loops/ex33-refactored.py
+++ python/python_the_hard_way/Exercise_33_While_Loops/ex33-refactored.py
@@ -1,18 +1,18 @@
 i = 0
 numbers = []
-def append_loop(list_count, safety_stop, list_var):
+def append_loop(list_count, safety_stop, list_var, increment):
 	while list_count < safety_stop:
 		print "At the top i is %d" % list_count
 		list_var.append(list_count)
-		list_count = list_count + 1
+		list_count = list_count + increment
 		print "Numbers now: ", numbers
 		print "At the bottom i is %d" % list_count
 
 local_list_var = numbers
 
-append_loop(i, 10, local_list_var)
+append_loop(i, 20, local_list_var, 2)
 
-print "The numbers: "
+print "The even numbers: "
 
 for num in local_list_var:
 	print num
@@ -21,9 +21,9 @@ for num in local_list_var:
 large_numbers = []
 Local_list_var = large_numbers
 
-append_loop(i, 20, local_list_var)
+append_loop(i, 100, local_list_var, 10)
 
-print "The numbers: "
+print "The large numbers: "
 
 for num in local_list_var:
 	print num

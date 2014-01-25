51551a3785ce809643bcc9ecf8486078420fbb58
Colin McNamara <colin@2cups.com>
2014-01-25 14:19:33 -0800

    got ex 36 game working. w00t!

--- python/python_the_hard_way/Exercise_36_Designing_and_Debugging/ex36-game.py
+++ python/python_the_hard_way/Exercise_36_Designing_and_Debugging/ex36-game.py
@@ -20,27 +20,6 @@ action_name = "empty"
 listname = "empty"
 user_choice = "empty"
 
-
-## this is work in progress and not used right now
-#def create_func_list_names(func_room_name):
-#	""" take the input room name and generate the appropriate list names"""
-#	global func_list_name
-#	global func_action_name
-
-#	list_append = "_list"
-#	action_append = "_action"
-
-#	func_list_name = (func_room_name + list_append)
-#	func_action_name = (func_room_name + action_append)
-
-#	func_list_name = room_conference_list
-#	func_action_name = room_conference_list_action
-
-#	print func_list_name
-#	print func_action_name
-
-
-
 def list_total_choices(func_listname):
 #	print "list_total_choices Name in func_Listname is %s" % func_listname
 	list_total_choices = len(func_listname)
@@ -95,6 +74,7 @@ def input_list_choice(func_listname_action):
 #		you_are_fired("You can't follow directions.")
 
 def room_function(func_room_name, func_room_statement, func_list_name, func_action_name):
+
 	""" iterate through all the functions per room """
 	global listname
 	listname = func_list_name
@@ -104,9 +84,9 @@ def room_function(func_room_name, func_room_statement, func_list_name, func_acti
 	# print all the choices 
 #	create_func_list_names(func_room_name)
 	# the two choices below are for debugging only
-	print "Room Function list = %s" % func_list_name
-	print "Room Function action = %s" % func_action_name
-	print "\n"
+#	print "Room Function list = %s" % func_list_name
+#	print "Room Function action = %s" % func_action_name
+#	print "\n"
 	list_total_choices(func_list_name)
 	print_all_choices_with_number(func_list_name)
 	input_list_choice(func_action_name)
@@ -114,18 +94,23 @@ def room_function(func_room_name, func_room_statement, func_list_name, func_acti
 #	print "Choosing index number %s from %s" % (user_choice, func_action_name)
 	action_name = func_action_name[user_choice]
 #	print "going to %s" % action_name
-	action_name()
+
+	func_room_name = []
+	func_room_statement = []
+	func_listname = []
+	func_action_name = []
+	action_name() 
 
 
 def you_are_fired():
 	""" successful end choice room """
-	print "This resulted in you being fired."
+	print "\nYou have bad judgement. This results in you being fired."
 	print "\n"
 	exit(0)
 
 def you_got_promoted():
 	""" failure end choice room """
-	print "Your boss and the community acknowledge how awesome you are and promote you."
+	print "\nYour boss and the community acknowledge how awesome you are and promote you."
 	print "\n"
 	exit(0)
 
@@ -151,67 +136,27 @@ def room_just_go():
 
 # room statement
 
-room_conference_statement = "You realize that you have to be at a conference tomorrow. What do you do? "
+room_conference_statement = "\nYou realize that you have to be at a conference tomorrow. What do you do? \n"
 room_conference_list = ["Call in sick", "Send an Employee", "Rush to get ready"]
 room_conference_list_action = [room_day_off, room_delegation, room_just_go]
 room_conference = ["room_room_conference", room_conference_statement, room_conference_list, room_conference_list_action]
 
-room_day_off_statement = "You called in Sick, taking the day off. What do you do? "
+room_day_off_statement = "\nYou called in Sick, taking the day off. What do you do? \n"
 room_day_off_list = ["Go Golfing", "Announce you are not avaible but continue to work", "turn off everything and hide"]
-room_day_off_list_action = [you_are_fired, room_delegation, room_start]
+room_day_off_list_action = [you_are_fired, room_delegation, you_are_fired]
 room_day_off = ["room_room_day_off", room_day_off_statement, room_day_off_list, room_day_off_list_action]
 
-room_delegation_statement = "You called in Sick, taking the day off. What do you do? "
-room_delegation_list = ["Go Golfing", "Announce you are not avaible but continue to work", "turn off everything and hide"]
-room_delegation_list_action = [room_delegation, room_delegation, room_just_go]
-room_delegation = ["room_room_day_off", room_day_off_statement, room_day_off_list, room_day_off_list_action]
+room_delegation_statement = "\nYou decided to delegate your work. What do you do? \n"
+room_delegation_list = ["Send your best guy", "send your worst guy", "send mediocre guy"]
+room_delegation_list_action = [you_are_fired, you_are_fired, you_got_promoted]
+room_delegation = ["room_delegation", room_delegation_statement, room_delegation_list, room_delegation_list_action]
 
+room_just_go_statement = "\nYou decided are just going. What do you do? "
+room_just_go_list = ["Take the first flight out", "Push your flight back an hour", "Push your flight back a day"]
+room_just_go_list_action = [you_are_fired, you_are_fired, you_are_fired]
+room_just_go = ["room_just_go", room_just_go_statement, room_just_go_list, room_just_go_list_action]
 
-you_are_fired_statement = "You realize that you have to be at a conference tomorrow. What do you do? "
+#you_are_fired_statement = "You realize that you have to be at a conference tomorrow. What do you do? \n"
 
-# Start - room_conference
-#room_function("room_room_conference",room_conference_statement, room_conference_list, room_conference_list_action)
-#room_function(*room_conference)
+# start the game
 room_start() 
-#create_func_list_names("room_room_conference")
-#print_func_list_names()
-#create_func_list_names("room_just_go")
-#print_func_list_names()
-
-
-## option call_in_sick
-#	print "You have %s choices" % room_conference_list(L)
-### goto room_day_off
-## option send_an_employee
-### goto room_delegation
-## option rush_to_get_ready
-### goto room_just_go
-
-
-# room_day_off
-## option go_golfing
-### goto fired_fail
-## option announce_available_continue_work
-### goto room_delegation
-## option hide
-### goto room_conference
-
-# room_just_go
-## option take_first_flight
-### goto fired_fail
-## option push_flight_one_hour
-### goto fired_fail
-## option push_flight_one_day
-### goto goto room_day_off
-
-# room_delegation
-## option send_best
-### goto fired_fail
-## option send_worst
-### goto fired_fail
-## option send_mediocre
-### goto promoted_win
-
-# end 1 - fired_fail
-
-# end 2 - promoted_win

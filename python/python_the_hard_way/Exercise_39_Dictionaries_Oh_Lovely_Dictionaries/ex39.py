4d1996771582b4754d2cfd86bac6f0bec82a4c4b
Colin McNamara <colin@2cups.com>
2014-01-27 06:07:15 -0800

    mid dictionaries (lesson 39) commit

---
+++ python/python_the_hard_way/Exercise_39_Dictionaries_Oh_Lovely_Dictionaries/ex39.py
@@ -0,0 +1,18 @@
+# Create a mapping of state to abbreviation
+states = {
+	'Oregon': 'OR',
+	'Florida': "FL",
+	'California': 'CA',
+	'New York': 'NY',
+	'Michigan': 'MI'
+}
+
+# Create a basic set of states and some cities in them
+
+cities = {
+	'CA': 'San Francisco',
+	'MI': 'Detroit',
+	'FL': 'Jacksonville'
+}
+
+# Add some more cities

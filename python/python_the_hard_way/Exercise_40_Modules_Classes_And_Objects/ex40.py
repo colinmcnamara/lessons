2252627119b9a1a8dc2d204a3f0e422ec1cab725
Colin McNamara <colin@2cups.com>
2014-01-28 05:04:55 -0800

    mid lesson 40

---
+++ python/python_the_hard_way/Exercise_40_Modules_Classes_And_Objects/ex40.py
@@ -0,0 +1,19 @@
+# dictionary with a key 'apple' you want then you do this :
+
+#mystuff = {'apple': "I AM APPLES!"}
+#print mystuff['apple']
+
+import mystuff
+
+# Necessary to load the apple function apparently. loads mystuff.apple as apple()
+mystuff.apple()
+
+def apple():
+	print "I AM APPLES!"
+
+print mystuff.tangerine
+
+
+#mystuff['apple'] # Get apple from a dict
+#mystuff.apple() # Get apple from the module mystuff
+#mystuff.tangerine # pull the variable tangerine from a module name mystuff
